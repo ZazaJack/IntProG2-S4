5454a0cf359cf3341e2670a76a6df50959f8fbe7
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-08 11:08:08 -0600

    Ejercicios de Py

---
+++ Sesion_1-2/concatenar_nombre.py
@@ -0,0 +1,7 @@
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+
+nombre_completo = nombre + " " + apellido
+
+print("Tu nombre completo es:", nombre_completo)
---
+++ Sesion_1-2/variables_numericas.py
@@ -0,0 +1,8 @@
+
+a = 6
+b = 10
+c = 7
+
+resultado = (a + b) * c / 2
+
+print("El resultado de la expresión es:", resultado)
