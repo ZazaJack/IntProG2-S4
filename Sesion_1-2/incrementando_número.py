d895f114d2f3afe78728fca508271006bd191f48
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-08 11:17:43 -0600

    Ejercicios de Py

---
+++ Sesion_1-2/incrementando_número.py
@@ -0,0 +1,8 @@
+
+texto_numero = "50"
+
+numero = int(texto_numero)
+
+numero += 1
+
+print("El número incrementado es:", numero)
---
+++ Sesion_1-2/porcentaje_número.py
@@ -0,0 +1,8 @@
+
+numero = float(input("Ingresa el número base: "))
+
+porcentaje = float(input("Ingresa el porcentaje que deseas calcular: "))
+
+resultado = (numero * porcentaje) / 100
+
+print(f"El {porcentaje}% de {numero} es: {resultado}")
