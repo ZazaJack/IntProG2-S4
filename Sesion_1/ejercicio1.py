658a5e1bb0465a9521dea43eeb1a3f1d2b65f6fe
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-08 08:11:59 -0600

    Ejercicios Py

---
+++ Sesion_1/ejercicio1.py
@@ -0,0 +1,13 @@
+nombre_completo = "Zahid Benjamín Reyes López"
+edad = 17
+altura = 1.75
+soltero = True
+
+print(f"Nombre completo:", {nombre_completo})
+print(f"Tipo de dato:", {type(nombre_completo)})
+print(f"Edad:", {edad})
+print(f"Tipo de dato:", {type(edad)})
+print(f"Altura:", {altura})
+print(f"Tipo de dato:", {type(altura)})
+print(f"Soltero:", {soltero})
+print(f"Tipo de dato:", {type(soltero)})
